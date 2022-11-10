02372f12e2c8cc323536b236eb9de1ab0475aa2f
SalmaSyawalan <salma.syawalan@gmail.com>
2022-11-10 11:39:04 +0700

    add new library and add comment

--- bot dc.py
+++ bot dc.py
@@ -1,9 +1,10 @@
 import pyautogui as auto
 import time
+import numpy as np
 
 time.sleep(5)
 
-
+#selama nilai true, akan menampikan string Eko
 while True :
     auto.write('EKO')
     auto.press('enter')
